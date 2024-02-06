873e275a43e907c39a6fe564876d4f660afd490a
aaron-yang799 <aaron.yang799@gmail.com>
2024-02-06 15:31:26 -0600

    added testing case for displayStudents()

--- Homework_2/src/edu/mu/Main.java
+++ Homework_2/src/edu/mu/Main.java
@@ -4,14 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Yippee!");
-		
-		StudentManager manager = new StudentManager();
-		
-		manager.readFromFile("studentData.txt");
-		
-		manager.searchStudentById(101);
-		
+		// Instantiate StudentManager, perform operations based on the requirements.
+		StudentManager studentManager = new StudentManager();
+		// Read student data from a file and initialize Student objects.
+		boolean fileReadStatus = studentManager.readFromFile("studentData.txt");
+		// Display all students.
+		System.out.println("displayStudents results: ");
+		studentManager.displayStudents();
+		// Search for a student by ID.
+		System.out.println("searchStudentByID results: ");
+		boolean studentFound = studentManager.searchStudentById(101);
+		// Update the grade of a student by ID.
 	}
 
 }
