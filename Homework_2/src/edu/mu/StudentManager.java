f82fc7a07f7079414929ff7522e9e430b24246c0
zachrsnyder <zachrsnyder2004@gmail.com>
2024-02-06 14:01:22 -0600

    Added readFromFile method and addToArray helper method to create a new instance of the array with the new student object added.

--- Homework_2/src/edu/mu/StudentManager.java
+++ Homework_2/src/edu/mu/StudentManager.java
@@ -1,5 +1,41 @@
 package edu.mu;
 
-public class StudentManager {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class StudentManager {
+	
+	Student[] students;
+	
+	public boolean readFromFile(String fileName) {
+		try {
+			FileInputStream inputStream = new FileInputStream(fileName);
+			
+			Scanner scanner = new Scanner(inputStream);
+	
+			while(scanner.hasNextLine()) {
+				int id = scanner.nextInt();
+				String name = scanner.next();
+				double grade = scanner.nextInt();
+				Student student = new Student(id, name, grade); 
+				students = addToArray(students, student);
+			};
+			scanner.close();
+			inputStream.close();
+			return true;
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	private Student[] addToArray(Student[] oldArray, Student newStudent)
+	{
+	    Student[] newArray = Arrays.copyOf(oldArray, oldArray.length+1);
+	    newArray[oldArray.length] = newStudent;
+	    return newArray;
+	}
 }
